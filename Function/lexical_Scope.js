a4cbf2425a5ba936c26fe08966f58a3b500e43c5
Gaurav Trivedi <trivedig319@gmail.com>
2023-09-16 03:06:14 +0530

    random-color-generator

--- Function/lexical_Scope.js
+++ Function/lexical_Scope.js
@@ -16,7 +16,7 @@ function outerMost(){
         innerMost();
     }    
     inner();
-
+    
 }
 
 outerMost();
---
+++ Projects/Rendom Color Generator/index.js
@@ -0,0 +1,40 @@
+const containerEl = document.querySelector(".container");
+const genRatorButton = document.getElementById("btn");
+
+genRatorButton.addEventListener('click', ()=>{
+    generateColors();
+})
+
+for(let i=0; i<30; i++){
+    const colorContainerEl = document.createElement("div");
+    colorContainerEl.classList.add("color-container");
+    containerEl.appendChild(colorContainerEl);
+}
+
+const colorContainerEls = document.querySelectorAll(".color-container");
+
+generateColors();
+
+function generateColors (){
+    colorContainerEls.forEach((el)=>{
+        const newColorCode = randomColor();   
+        // console.log("#"+newColorCode);
+        el.style.backgroundColor = "#"+newColorCode;
+        el.innerText = '#'+newColorCode;
+    });
+}
+
+
+function randomColor (){
+    const chars = "0123456789abcdef";
+    const colorCodeLength = 6;
+    let colorCode = "";
+
+    for(let index=0; index<colorCodeLength; index++){
+        const randomNum = Math.floor(Math.random()*chars.length);
+        colorCode += chars.substring(randomNum, randomNum+1);
+        // console.log(randomNum, colorCode);
+    }
+    return colorCode;
+}
+
